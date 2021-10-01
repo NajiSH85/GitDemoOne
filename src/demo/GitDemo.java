aead621946e1619e30103850c5b64a5dfae9a902
shams <shams@10.0.0.5>
2021-09-30 18:13:06 -0700

    Added by SK

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -5,6 +5,14 @@ public class GitDemo {
 	public static void main(String[]args) {
 		System.out.println("Git Demooo...");
 		
+		
+		boolean isSunday = true;
+		
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("xyz");
+		}
 	}
 
 }
